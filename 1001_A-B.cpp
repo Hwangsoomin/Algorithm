be09bc344b8403a395545c50da1ecd3dfdb57911
Hwangsoomin <43667316+Hwangsoomin@users.noreply.github.com>
2019-06-08 15:15:02 +0900

    Create 1001_A-B.cpp

---
+++ 1001_A-B.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<string>
+using namespace std;
+int a, b;
+int main(void) {
+	scanf("%d%d", &a, &b);
+	printf("%d\n", a - b);
+	return 0;
+}

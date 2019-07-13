56b00118e76d2f93f972d1f1681eafe868eb56f4
Hwangsoomin <43667316+Hwangsoomin@users.noreply.github.com>
2019-07-13 20:03:12 +0900

    Create 17294_귀여운 수.cpp

---
+++ 17294_귀여운 수.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<cstring>
+using namespace std;
+long long num;
+int arr[25], nn, pre;
+bool flag = false;
+int main(void) {
+	scanf("%lld", &num);
+	while (num > 0) {
+		arr[nn++] = num % 10;
+		num /= 10;
+	}
+	pre = arr[1] - arr[0];
+	for (int i = 1; i < nn - 1; i++) {
+		int aa = arr[i + 1] - arr[i];
+		if (pre != aa) {
+			flag = true;
+			break;
+		}
+	}
+	if (flag) printf("흥칫뿡!! <(￣ ﹌ ￣)>\n");
+	else printf("◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!\n");
+	return 0;
+}

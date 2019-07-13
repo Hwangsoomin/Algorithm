997e4806ff9247a468dff8af5ad3cc835d302195
Hwangsoomin <43667316+Hwangsoomin@users.noreply.github.com>
2019-07-13 20:24:32 +0900

    Create 17262_팬덤이넘쳐흘러.cpp

---
+++ 17262_팬덤이넘쳐흘러.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<cstring>
+using namespace std;
+int n, s, e, Min = 1e9, Max;
+int main(void) {
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d%d", &s, &e);
+		Min = min(Min, e);
+		Max = max(Max, s);
+	}
+	if (Max >= Min)printf("%d\n", Max - Min);
+	else printf("0\n");
+	return 0;
+}

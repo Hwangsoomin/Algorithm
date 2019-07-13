0c2221903f9f265c455d30e5201690e39c5f2ab4
Hwangsoomin <43667316+Hwangsoomin@users.noreply.github.com>
2019-07-13 21:59:08 +0900

    Create 2033_반올림.cpp

---
+++ 2033_반올림.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<cstring>
+using namespace std;
+long long n;
+int arr[10];
+int main(void) {
+	int a = 0, num = 10, num1 = 1;
+	scanf("%lld", &n);
+	while (n > num) {
+		long long tmp = n % num;
+		if (tmp >= 5 * num1) {
+			n = n - tmp;
+			n += num;
+		}
+		else n = n - tmp;
+		num *= 10;
+		num1 *= 10;
+	}
+	printf("%lld\n", n);
+
+	return 0;
+}

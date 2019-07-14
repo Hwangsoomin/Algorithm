a6106a6d48270b2d6713a9eae9125c53256730e3
Hwangsoomin <43667316+Hwangsoomin@users.noreply.github.com>
2019-07-14 18:07:24 +0900

    Create 8974_희주의수학시험.cpp

---
+++ 8974_희주의수학시험.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<cstring>
+#include<string>
+using namespace std;
+int A, B, sum, num, pre, i = 1;
+int arr[1005];
+bool flag = false;
+int main(void) {
+	scanf("%d%d", &A, &B);
+	pre = 1, num = 1;
+	for (int i = 1; i <= 1005; i++)arr[i] = i;
+	while(1) {
+		num += arr[i];
+		if (pre <= B) {
+			for (int j = pre; j < num; j++) {
+				if (j >= A && j <= B) {
+					sum += i;
+					if (j == B) {
+						flag = true;
+						break;
+					}
+				}
+			}
+		}
+		if (flag)break;
+		pre = num;
+		i++;
+	}
+	printf("%d\n", sum);
+	return 0;
+}

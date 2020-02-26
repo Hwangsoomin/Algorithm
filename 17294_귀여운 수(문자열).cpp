cb89e37ee2dbeda0cb36e09667be388510991ee1
Hwangsoomin <maum97@naver.com>
2020-02-26 21:18:44 +0900

    Create 17294_귀여운 수(문자열).cpp

---
+++ 17294_귀여운 수(문자열).cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#include<utility>
+#include<math.h>
+#include<set>
+#include<string>
+#include<queue>
+#include<stack>
+#include<map>
+#define ll long long
+using namespace std;
+string str;
+bool flag = false;
+int main(void) {
+	cin >> str;
+	if (str.size() <= 2)printf("◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!\n");
+	else {
+		int num = str[1] - str[0];
+		for (int i = 2; i < str.size(); i++) {
+			int nnum = str[i] - str[i - 1];
+			if (nnum != num) {
+				flag = true;
+				break;
+			}
+		}
+		if (!flag)printf("◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!\n");
+		else printf("흥칫뿡!! <(￣ ﹌ ￣)>\n");
+	}
+	return 0;
+}

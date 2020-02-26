2262542ead2508e1c3185cea03c7e3edf5a82489
Hwangsoomin <maum97@naver.com>
2020-02-26 21:18:37 +0900

    Create 10250_ACM호텔.cpp

---
+++ 10250_ACM호텔.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#include<utility>
+#include<math.h>
+#include<set>
+#include<string>
+#include<queue>
+#include<stack>
+#include<map>
+#define ll long long
+using namespace std;
+int h, w, n, t;
+int main(void) {
+	scanf("%d", &t);
+	while (t--) {
+		scanf("%d%d%d", &h, &w, &n);
+		int num = n / h;
+		int num1 = n % h;
+		int ans = 0;
+		if (num1 == 0)ans = h * 100 + num;
+		else ans = num1 * 100 + num + 1; 
+		printf("%d\n", ans);
+	}
+	return 0;
+}

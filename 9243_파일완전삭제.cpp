3f314fb7458e4b802b4e8dbe9549276deb43738b
Hwangsoomin <maum97@naver.com>
2020-03-01 01:56:49 +0900

    Create 9243_파일완전삭제.cpp

---
+++ 9243_파일완전삭제.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#include<utility>
+#include<math.h>
+#include<set>
+#include<string>
+#include<queue>
+#include<stack>
+#include<map>
+#define ll long long
+using namespace std;
+int n;
+string str, str1, ans;
+bool flag = false;
+int main(void) {
+	scanf("%d", &n);
+	cin >> str >> str1;
+	if (n % 2 != 0) {
+		for (int i = 0; i < str.size(); i++) {
+			if (str[i] == '1')ans += '0';
+			else ans += '1';
+		}
+		if (ans == str1)flag = true;
+	}
+	else {
+		if (str == str1)flag = true;
+	}
+	if (flag)printf("Deletion succeeded\n");
+	else printf("Deletion failed\n");
+	return 0;
+}

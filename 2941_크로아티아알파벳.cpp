c8c86c78b6de0baac69a37d297a83099ef46a03a
Hwangsoomin <maum97@naver.com>
2020-03-01 01:41:06 +0900

    Create 2941_크로아티아알파벳.cpp

---
+++ 2941_크로아티아알파벳.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#include<utility>
+#include<math.h>
+#include<set>
+#include<string>
+#include<queue>
+#include<stack>
+#include<map>
+#define ll long long
+using namespace std;
+string str;
+int ans;
+int main(void) {
+	cin >> str;
+	for (int i = 0; i < str.size();) {
+		int flag = 0;
+		if (str[i] == 'c' && str[i + 1] == '=') flag = 2;
+		else if (str[i] == 'c' && str[i + 1] == '-') flag = 2;
+		else if (str[i] == 'd' && str[i + 1] == 'z' && str[i + 2] == '=') flag = 3;
+		else if (str[i] == 'd' && str[i + 1] == '-') flag = 2;
+		else if (str[i] == 'l' && str[i + 1] == 'j') flag = 2;
+		else if (str[i] == 'n' && str[i + 1] == 'j') flag = 2;
+		else if (str[i] == 's' && str[i + 1] == '=') flag = 2;
+		else if (str[i] == 'z' && str[i + 1] == '=') flag = 2;
+		else flag = 1;
+		ans += 1;
+		i += flag;
+	}
+	printf("%d\n", ans);
+	return 0;
+}

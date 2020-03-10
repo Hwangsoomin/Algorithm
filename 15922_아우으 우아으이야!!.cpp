72efb9e18ac1a30eabc4d2c7bf57ef2134fda777
Hwangsoomin <maum97@naver.com>
2020-03-10 23:33:24 +0900

    Create 15922_아우으 우아으이야!!.cpp

---
+++ 15922_아우으 우아으이야!!.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#define ll long long
+using namespace std;
+ll n, sum, pre;
+pair<ll, ll> line[100005];
+int main(void) {
+	ll a, b;
+	scanf("%lld", &n);
+	for (int i = 1; i <= n; i++) {
+		scanf("%lld%lld", &a, &b);
+		line[i] = { a,b };
+	}
+	pre = line[1].first;
+	for (int i = 1; i <= n; i++) {
+		if (pre <=line[i].first)sum += (line[i].second - line[i].first);
+		else {
+			if (pre < line[i].second)sum += (line[i].second - pre);
+		}
+		if (pre < line[i].second) pre = line[i].second;
+	}
+	printf("%lld\n", sum);
+	return 0;
+}

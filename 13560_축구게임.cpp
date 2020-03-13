e844318ace92338d97a91e968795813e652fe3e1
Hwangsoomin <maum97@naver.com>
2020-03-14 03:34:57 +0900

    Create 13560_축구게임.cpp

---
+++ 13560_축구게임.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<math.h>
+#define ll long long
+using namespace std;
+int soc[10005], sum, n;
+bool flag = false;
+int main(void) {
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)scanf("%d", &soc[i]);
+	sort(soc + 1, soc + 1 + n);
+	for (int i = 1; i <= n; i++) {
+		sum += soc[i];
+		if (sum < ((i - 1) * i) / 2) {
+			printf("-1\n");
+			flag = true;
+			break;
+		}
+	}
+	if (!flag) {
+		if (sum == ((n - 1) * n) / 2)printf("1\n");
+		else printf("-1\n");
+	}
+	return 0;
+}

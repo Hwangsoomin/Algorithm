9e9438b1072f71991626d6086e497380e2a5aac2
Soomin <43667316+Hwangsoomin@users.noreply.github.com>
2021-03-11 15:59:11 +0900

    Create 20944_팰린드롬척화비.cpp

---
+++ 20944_팰린드롬척화비.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+#include<queue> 
+#include<cmath>
+#include<map>
+#include<climits>
+#define ll long long
+using namespace std;
+int n;
+string str;
+int main(void) {
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)str += 'a';
+	cout << str << endl;
+	return 0;
+}

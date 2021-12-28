74e789ce9d53534639f314e9a72731bf8078a363
Hwangsoomin <maum97@naver.com>
2021-12-28 18:26:23 +0900

    Update 5014_스타트링크.cpp

--- 백준/5014_스타트링크.cpp
+++ 백준/5014_스타트링크.cpp
@@ -1,39 +1,43 @@
-#include<iostream>
-#include<cstring>
-#include<algorithm>
-#include<vector>
-#include<queue>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <map>
+#include <stack>
+#include <cmath>
+#include <deque>
 using namespace std;
-int f, s, g, u, d;
+int F, S, G, U, D, answer;
 int check[1000005];
-queue<pair<int, int>> q;
-bool flag = false;
+int dr[4]={-1, 0, 1, 0};
+int dc[4]={0, 1, 0, -1};
+queue<int> q;
+vector<int> ele;
+void bfs() {
+    q.push(S);
+    check[S] = 1;
+    while(!q.empty()) {
+        int here = q.front();
+        q.pop();
+        for(int i=0; i<ele.size(); i++) {
+            int next = here + ele[i];
+            if(next<1 || next>F) continue;
+            if(!check[next]) {
+                check[next] = check[here] + 1;
+                q.push(next);
+            }
+        }
+        
+    }
+}
 int main(void) {
-	scanf("%d%d%d%d%d", &f, &s, &g, &u, &d);
-	q.push({ s,0 });
-	while (!q.empty()) {
-		int here = q.front().first;
-		int cnt = q.front().second;
-		check[here] = 1;
-		if (here == g) {
-			printf("%d\n", cnt);
-			flag = true;
-			break;
-		}
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int next1 = here + u;
-			int next2 = here - d;
-			if (!check[next1] && 1 <= next1 && next1 <= f) {
-				q.push({ next1,cnt + 1 });
-				check[next1] = 1;
-			}
-			if (!check[next2] && 1 <= next2 && next2 <= f) {
-				q.push({ next2,cnt + 1 });
-				check[next2] = 1;
-			}
-		}
-	}
-	if (!flag)printf("use the stairs\n");
-	return 0;
+    scanf("%d%d%d%d%d", &F, &S, &G, &U, &D);
+    ele.push_back(U);
+    ele.push_back(-D);
+    bfs();
+    answer = check[G] - 1;
+    if(answer==-1) printf("use the stairs\n");
+    else printf("%d\n", answer);
+    return 0;
 }
